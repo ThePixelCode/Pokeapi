57614560fc0159fda6d9834c602f63004ecaa531
ThePixelCode <thepixelcode@proton.me>
2024-05-19 15:13:27 -0500

    lib: created lib.rs

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,7 @@
+const POKEAPI_BASE_URL: &str = "https://pokeapi.co/api/v2";
+
+#[derive(Debug, Default, PartialEq, Eq, Clone, serde::Serialize, serde::Deserialize)]
+pub struct NamedAPIResource {
+    name: String,
+    url: String,
+}
