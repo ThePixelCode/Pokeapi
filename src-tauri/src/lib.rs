a70c42cb1ec00af550d620efc3f07fb7017bcb9d
ThePixelCode <thepixelcode@proton.me>
2024-05-19 15:15:31 -0500

    search: Added search.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod search;
+
 const POKEAPI_BASE_URL: &str = "https://pokeapi.co/api/v2";
 
 #[derive(Debug, Default, PartialEq, Eq, Clone, serde::Serialize, serde::Deserialize)]
---
+++ src-tauri/src/search.rs
@@ -0,0 +1,57 @@
+#[derive(Debug, Clone, PartialEq, Eq, Default, serde::Serialize, serde::Deserialize)]
+pub enum SearchOption {
+    ID(u32),
+    Name(String),
+    #[default]
+    None,
+}
+
+pub trait NamedSearch: From<SearchOption> + Default {
+    fn set_option(self, option: SearchOption) -> Self;
+
+    fn set_id(self, id: u32) -> Self {
+        self.set_option(SearchOption::ID(id))
+    }
+
+    fn set_name(self, name: String) -> Self {
+        self.set_option(SearchOption::Name(name))
+    }
+
+    fn get_option(&self) -> Option<&SearchOption>;
+
+    fn get_id(&self) -> Option<&u32> {
+        let op = self.get_option()?;
+        match op {
+            SearchOption::ID(id) => Some(id),
+            SearchOption::Name(_) => None,
+            SearchOption::None => None,
+        }
+    }
+
+    fn get_name(&self) -> Option<&String> {
+        let op = self.get_option()?;
+        match op {
+            SearchOption::ID(_) => None,
+            SearchOption::Name(name) => Some(name),
+            SearchOption::None => None,
+        }
+    }
+}
+pub trait UnnamedSearch: From<SearchOption> + Default {
+    fn set_option(self, option: SearchOption) -> Self;
+
+    fn set_id(self, id: u32) -> Self {
+        self.set_option(SearchOption::ID(id))
+    }
+
+    fn get_option(&self) -> Option<&SearchOption>;
+
+    fn get_id(&self) -> Option<&u32> {
+        let op = self.get_option()?;
+        match op {
+            SearchOption::ID(id) => Some(id),
+            SearchOption::Name(_) => None,
+            SearchOption::None => None,
+        }
+    }
+}
