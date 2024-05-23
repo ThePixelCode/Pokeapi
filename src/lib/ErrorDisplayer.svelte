880cb2ed875947f73074f6d778cfcc24a59456ae
ThePixelCode <thepixelcode@proton.me>
2024-05-22 21:27:24 -0500

    fe: errorDisplay: Added display component

---
+++ src/lib/ErrorDisplayer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let error: string;
+</script>
+
+<div class="p-2">
+    <h1 class="text-2xl">Error found!</h1>
+    <p>The error was: <span class="text-red-600">{error}</span></p>
+</div>
