19e16b3bb6bb89523252f79092733a6edcb3556d
ThePixelCode <thepixelcode@proton.me>
2024-05-23 20:20:14 -0500

    fe: main-window: Created main window

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
   import Header from "./lib/Header.svelte";
+  import Search from "./lib/Search.svelte";
+  import RenderPokemon from "./lib/pokemon/RenderPokemon.svelte";
+  import type { Pokemon } from "./lib/types/pokemon";
+
+  let pokemon: Pokemon | undefined = undefined;
 </script>
 
 <Header />
+
+<div class="grid grid-cols-2">
+  <div>
+    <Search
+      callback={async (val) => {
+        pokemon = await invoke("get_pokemon", { searchOptions: val });
+      }}
+      searchFor="Pokemon"
+    />
+  </div>
+  {#if pokemon !== undefined}
+    <div class="bg-neutral-200 dark:bg-neutral-700 m-5">
+      <RenderPokemon {pokemon} />
+    </div>
+  {/if}
+</div>
